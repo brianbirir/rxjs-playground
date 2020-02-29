157eb14b9cd9c746173d6ad9340846aee56c207c
brianbirir <brianbirir@gmail.com>
2020-02-29 21:12:09 +0300

    Use node module bootstrap

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
-import { Observable } from 'rxjs';
+import { Observable } from "rxjs";
+import "bootstrap";
 
 var observable = Observable.create((observer: any) => {
-    observer.next('Hello World!');
-    observer.next('Hello Again!');
-    observer.complete();
-    observer.next('Bye');
-
-})
+  observer.next("Hello World!");
+  observer.next("Hello Again!");
+  observer.complete();
+  observer.next("Bye");
+});
 
 observable.subscribe(
-    (x :any) => logItem(x),
-    (error: any) => logItem("Error: " + error),
-    () => logItem('Completed'),
-)
+  (x: any) => logItem(x),
+  (error: any) => logItem("Error: " + error),
+  () => logItem("Completed")
+);
 
 function logItem(val: any) {
-    var node = document.createElement("li");
-    node.classList.add("list-group-item");
-    var textNode = document.createTextNode(val);
-    node.appendChild(textNode);
-    document.getElementById("list").appendChild(node);
+  var node = document.createElement("li");
+  node.classList.add("list-group-item");
+  var textNode = document.createTextNode(val);
+  node.appendChild(textNode);
+  document.getElementById("list").appendChild(node);
 }
