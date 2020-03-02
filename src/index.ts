54077208b7fa2ca83bc8492f4af156f4be393773
brianbirir <brianbirir@gmail.com>
2020-03-02 10:20:26 +0300

    Observables learnings

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import { Observable, of, from, fromEvent, concat } from "rxjs";
-import "bootstrap";
+import { Observable, of, from, fromEvent, concat } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import 'bootstrap';
 
-import { allBooks, allReaders } from "../data/books";
+import { allBooks, allReaders } from '../data/books';
 
 // let allBooksObservable$ = Observable.create((subscriber: any) => {
 //   if (document.title !== "RxBookTracker") {
@@ -19,12 +20,43 @@ import { allBooks, allReaders } from "../data/books";
 
 // allBooksObservable$.subscribe((book: any) => console.log(book.title));
 
-let source1$ = of("hello", 10, true, allReaders[0].name);
+// OF()
+
+// let source1$ = of("hello", 10, true, allReaders[0].name);
 
 // source1$.subscribe((value: any) => console.log(value));
 
-let source2$ = from(allBooks);
+// FROM()
+
+// let source2$ = from(allBooks);
 
 // source2$.subscribe((value: any) => console.log(value));
 
-concat(source1$, source2$).subscribe((value: any) => console.log(value));
+// CONCAT()
+
+// concat(source1$, source2$).subscribe((value: any) => console.log(value));
+
+// let button = document.getElementById('readersButton');
+
+// FROMEVENT() DOM stream
+
+// fromEvent(button, 'click').subscribe(event => {
+//   console.log(event);
+//   let readersDiv = document.getElementById('readers');
+
+//   for (let reader of allReaders) {
+//     readersDiv.innerHTML += reader.name + '<br>';
+//   }
+// });
+
+let button = document.getElementById('readersButton');
+
+fromEvent(button, 'click').subscribe(event => {
+  ajax('/data').subscribe(ajaxResponse => {
+    console.log(ajaxResponse);
+    let readersDiv = document.getElementById('readers');
+    for (let reader of allReaders) {
+      readersDiv.innerHTML += reader.name + '<br>';
+    }
+  });
+});
